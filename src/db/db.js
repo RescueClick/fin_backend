cda4c5e6ac7ab8f338355c3e5c0486da0567f1ce
Prachi <pn71900@gmail.com>
2026-02-25 10:37:13 +0530

    updated new backend changes

--- src/db/db.js
+++ src/db/db.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
 export async function connectDB(uri) {
+  console.log("Connecting to MongoDB...", uri);
   mongoose.set("strictQuery", true);
   await mongoose.connect(uri);
   console.log("✓ MongoDB connected");
